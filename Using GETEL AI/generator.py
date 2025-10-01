22a608f5227965eaaa8e6dcc461bbad75c889dbc
Vishwa Randy GW <136871744+vishwagw@users.noreply.github.com>
2025-10-01 15:30:12 +0530

    Update generator.py

--- Using GETEL AI/generator.py
+++ Using GETEL AI/generator.py
@@ -2,3 +2,12 @@
 import pandas as pd
 from gretel_client import configure_session
 from gretel_trainer.relational import RelationalData
+
+# configuring api keys:
+configure_session(api_key="your_api_key", cache="yes", validate=True)
+
+# creating data requirements:
+csv_dir = "csv/"
+tables = [("patients", "Patient ID"), ("lab_results", "Lab Result ID"), ("treatments", "Treatment ID")]
+foreign_keys = [("lab_results.Patient ID", "patients.Patient ID"), ("treatments.Patient ID", "patients.Patient ID")]
+
