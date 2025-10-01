df08fa4e7e42e7502b1c5dead8ab110a65b549d7
Vishwa Randy GW <136871744+vishwagw@users.noreply.github.com>
2025-10-01 15:29:07 +0530

    Create generator.py

---
+++ Using GETEL AI/generator.py
@@ -0,0 +1,4 @@
+# gretel AI implementation for synthetic data generation sample code:
+import pandas as pd
+from gretel_client import configure_session
+from gretel_trainer.relational import RelationalData
